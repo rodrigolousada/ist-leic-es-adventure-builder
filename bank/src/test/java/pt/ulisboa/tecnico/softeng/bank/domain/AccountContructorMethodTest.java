d617e41957da1a2046ad1b2a9cbbef05ee7dff3d
João Ricardo Matos <joao.r.matos@ist.utl.pt>
2017-03-10 12:39:44 +0000

    Add new tests for Account constructor

--- bank/src/test/java/pt/ulisboa/tecnico/softeng/bank/domain/AccountContructorMethodTest.java
+++ bank/src/test/java/pt/ulisboa/tecnico/softeng/bank/domain/AccountContructorMethodTest.java
@@ -5,6 +5,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
+
 public class AccountContructorMethodTest {
 	Bank bank;
 	Client client;
@@ -25,6 +27,22 @@ public class AccountContructorMethodTest {
 		Assert.assertEquals(0, account.getBalance());
 		Assert.assertEquals(1, this.bank.getNumberOfAccounts());
 		Assert.assertTrue(this.bank.hasClient(this.client));
+	}
+
+	@Test(expected = BankException.class)
+	public void nullBankArgument() {
+		Account account = new Account(null, this.client);
+	}
+
+	@Test(expected = BankException.class)
+	public void nullClientArgument() {
+		Account account = new Account(this.bank, null);
+	}
+
+	@Test(expected = BankException.class)
+	public void clientNotInBank() {
+		Bank otherBank = new Bank("Money2", "BK02");
+		Account account = new Account(otherBank, this.client);
 	}
 
 	@After
