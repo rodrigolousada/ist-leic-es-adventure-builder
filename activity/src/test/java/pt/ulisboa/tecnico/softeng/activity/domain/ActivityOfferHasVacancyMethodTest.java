fe17ac5d455478b9ebbd5e700b8bed2e7301f421
André Mendonça <andremendonca94@gmail.com>
2017-03-18 17:30:33 +0000

    ActivityOfferHasVacancyMethod.java

--- activity/src/test/java/pt/ulisboa/tecnico/softeng/activity/domain/ActivityOfferHasVacancyMethodTest.java
+++ activity/src/test/java/pt/ulisboa/tecnico/softeng/activity/domain/ActivityOfferHasVacancyMethodTest.java
@@ -6,6 +6,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import pt.ulisboa.tecnico.softeng.activity.domain.exception.ActivityException;
+
 public class ActivityOfferHasVacancyMethodTest {
 	private ActivityOffer offer;
 
@@ -13,16 +15,37 @@ public class ActivityOfferHasVacancyMethodTest {
 	public void setUp() {
 		ActivityProvider provider = new ActivityProvider("XtremX", "ExtremeAdventure");
 		Activity activity = new Activity(provider, "Bush Walking", 18, 80, 3);
-
+		
 		LocalDate begin = new LocalDate(2016, 12, 19);
 		LocalDate end = new LocalDate(2016, 12, 21);
 
 		this.offer = new ActivityOffer(activity, begin, end);
+		
 	}
 
 	@Test
 	public void successZeroBookinks() {
 		Assert.assertTrue(this.offer.hasVacancy());
+	}
+	
+	@Test (expected = ActivityException.class)
+	public void notSucess(){
+		ActivityProvider provider = new ActivityProvider("XtremX", "ExtremeAdventure");
+		ActivityProvider provider2 = new ActivityProvider("XPTO", "XPTO");
+		
+		Activity activity = new Activity(provider, "Bush Walking", 18, 80, 1);
+		
+		LocalDate begin = new LocalDate(2016, 12, 19);
+		LocalDate end = new LocalDate(2016, 12, 21);
+
+		this.offer = new ActivityOffer(activity, begin, end);
+		
+		Booking booking = new Booking(provider,this.offer);
+		this.offer.addBooking(booking);
+		Booking booking2 = new Booking(provider2,this.offer);
+		this.offer.addBooking(booking2);
+		
+		Assert.assertFalse(this.offer.hasVacancy());
 	}
 
 	@After
