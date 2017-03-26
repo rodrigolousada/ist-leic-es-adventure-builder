044a8e15f0882f4e64ec7b1dcabe9fbf1af2d59a
Diegues <andre.diegues.f@gmail.com>
2017-03-26 17:31:50 +0100

    Week 1 ReserveActivityState closes #4

--- broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/Adventure.java
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/Adventure.java
@@ -178,7 +178,7 @@ public class Adventure {
 			this.state = null;
 			break;
 		case RESERVE_ACTIVITY:
-			this.state = null;
+			this.state = new ReserveActivityState();
 			break;
 		case BOOK_ROOM:
 			this.state = new BookRoomState();
@@ -220,24 +220,7 @@ public class Adventure {
 
 			break;
 		case RESERVE_ACTIVITY:
-			try {
-				this.activityConfirmation = ActivityInterface.reserveActivity(this.begin, this.end, this.age);
-			} catch (ActivityException ae) {
-				setState(State.UNDO);
-			} catch (RemoteAccessException rae) {
-				// increment number of errors
-				// if (number of errors == 5) {
-				// adventure.setState(State.UNDO);
-				// }
-				// return;
-			}
-
-			if (this.begin.equals(this.end)) {
-				setState(State.CONFIRMED);
-			} else {
-				setState(State.BOOK_ROOM);
-			}
-
+			this.state.process(this);
 			break;
 		case BOOK_ROOM:
 			this.state.process(this);
---
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/ReserveActivityState.java
@@ -0,0 +1,35 @@
+package pt.ulisboa.tecnico.softeng.broker.domain;
+
+import pt.ulisboa.tecnico.softeng.activity.exception.ActivityException;
+import pt.ulisboa.tecnico.softeng.broker.domain.Adventure.State;
+import pt.ulisboa.tecnico.softeng.broker.exception.RemoteAccessException;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.ActivityInterface;
+
+public class ReserveActivityState extends AdventureState {	
+
+	@Override
+	public void process(Adventure adventure) {
+		try {
+			adventure.setActivityConfirmation( ActivityInterface.reserveActivity(adventure.getBegin(), adventure.getEnd(), adventure.getAge()));
+		} catch (ActivityException ae) {
+			adventure.setState(State.UNDO);
+		} catch (RemoteAccessException rae) {
+			// increment number of errors
+			// if (number of errors == 5) {
+			// adventure.setState(State.UNDO);
+			// }
+			// return;
+		}
+	
+		if (adventure.getBegin().equals(adventure.getEnd())) {
+			adventure.setState(State.CONFIRMED);
+		} else {
+			adventure.setState(State.BOOK_ROOM);
+		}
+	}
+
+	@Override
+	public State getState() {
+		return State.RESERVE_ACTIVITY;
+	}
+}
