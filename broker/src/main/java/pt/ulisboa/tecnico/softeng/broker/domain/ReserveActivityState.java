472793267cbe1141563e074dfa7cf0500136c533
Diegues <andre.diegues.f@gmail.com>
2017-04-02 15:22:34 +0100

    Final ReserveAcitivityStateProcessMethodTest closes #5

--- broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/ReserveActivityState.java
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/ReserveActivityState.java
@@ -13,12 +13,13 @@ public class ReserveActivityState extends AdventureState {
 			adventure.setActivityConfirmation( ActivityInterface.reserveActivity(adventure.getBegin(), adventure.getEnd(), adventure.getAge()));
 		} catch (ActivityException ae) {
 			adventure.setState(State.UNDO);
+			return;
 		} catch (RemoteAccessException rae) {
 			// increment number of errors
 			// if (number of errors == 5) {
 			// adventure.setState(State.UNDO);
 			// }
-			// return;
+			 return;
 		}
 	
 		if (adventure.getBegin().equals(adventure.getEnd())) {
